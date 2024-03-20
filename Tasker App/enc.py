48cc752a211aae8bf9b1c7918854f2bc545bffd3
tniromin <tniromin2@gmail.com>
2024-03-20 09:25:59 +0530

    Quick Commmit

---
+++ Tasker App/enc.py
@@ -0,0 +1,25 @@
+class encrypt:
+
+    msg = "" # original Message
+    buffer = "" # Buffer
+    eMsg = "" # Encrypted message
+
+    def f(self):
+        return 0
+    
+    def translate(self,n,l):
+        print()
+    
+    def ext(self):
+        n=input("Enter Message")
+        x=0
+        c=0
+        while n[x] == n[x+1]:
+            x=x+1
+        
+            
+            
+     
+    
+X = encrypt()
+print(x.ext())
